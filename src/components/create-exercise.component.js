30f966e8f8734b29fcbdfff4e44a8837dc4b5915
lennoxstark47 <twisamhaldar@gmail.com>
2021-07-23 18:41:01 +0530

    added router and frontend

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class CreateExercise extends Component {
+	render() {
+		return (
+			<div>
+				<p>
+					You are on the create exercise component!
+				</p>
+			</div>
+		);
+	}
+}
